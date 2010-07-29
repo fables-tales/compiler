3cd2e9a58056cefaeaa1aa936a2f9ec0b58342c7
Sam Phippen <samphippen@googlemail.com>
2010-07-30 00:24:46 +0100

    more lexer hacking

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -76,18 +76,64 @@ stringTokens = [
                         ("e", TokenE)
                ]
 
+--extracts a token from a string from the above simple strings map
 getTokenForString :: String -> ((Maybe Token), Int)
 getTokenForString s = let pair = (find ((`isPrefixOf` s) . fst) stringTokens) in
                         if pair == Nothing then (Nothing, 0)
                         else let certainPair = certain pair in
                             (Just (snd certainPair), length (fst certainPair))
 
-lexer :: String -> [Token]
-lexer s | (fst (getTokenForString s) /= Nothing) = let tokString = getTokenForString s in
-                                                    certain (fst (tokString)) : lexer (drop (snd tokString) s)
-lexer [] = []
+--define the quote character magic constants all over the place suck
+stringDelimiter :: Char
+stringDelimiter = '\''
+
 --turn a maybe monad into a certain value or error
 certain :: Maybe a -> a
 certain (Just a) = a
 certain Nothing = error "Tried to make a Nothing monad certain"
 
+--useful string literal handler function
+
+--gets the index of the last quote for string literals
+endQuoteIndex :: [Int] -> Maybe Int
+endQuoteIndex (a : b : rest) = if (a + 1) /= b then Just a else endQuoteIndex rest
+endQuoteIndex (a : []) = Just a
+endQuoteIndex _ = Nothing
+
+--gets a prefix of at most size n from an array, doesn't fail if there aren't that many elements
+getPrefix :: Int -> [a] -> [a]
+getPrefix 0 array = []
+getPrefix n [] = []
+getPrefix n (a : rest) = a : getPrefix (n - 1) rest
+
+innerStringLiteral :: String -> String
+innerStringLiteral [] = []
+innerStringLiteral ('\'' : '\'' : rest) = '\'' : innerStringLiteral rest
+innerStringLiteral (c : rest) = c : innerStringLiteral rest
+
+getStringLiteral :: String -> ((Maybe String), Int)
+getStringLiteral s | (head s) == '\'' = let end = endQuoteIndex (elemIndices '\'' (drop 1 s)) in
+                                        if end == Nothing then (Nothing, 0)
+                                        else  (Just (innerStringLiteral (getPrefix (certain end) (drop 1 s))), certain end)
+getStringLiteral _ = (Nothing, 0)
+
+-- Int parameter is used for line counting
+lexer :: String -> Int -> [Token]
+lexer s lineCount | s == [] = []
+                  | (fst (getTokenForString (map toLower s)) /= Nothing) =
+                                                    let tokString = getTokenForString s in
+                                                    certain (fst (tokString)) : lexer (drop (snd tokString) s) lineCount
+
+                  | head s == '\n' = lexer (drop 1 s) (lineCount + 1)
+                  --drop whitespace if we're not in a string
+                  | isWhiteSpace (head s) = lexer (drop 1 s) lineCount
+                  --if we get an alpha we're at an identifier
+                  | isAlpha (head s) = let x = (span (isAlphaNum) s) in TokenIdentifier (fst x) : lexer (snd x) lineCount
+                  -- integer constant lexer
+                  | isDigit (head s) = let x = span (isDigit) s in TokenIntLiteral (read (fst x) :: Int) : lexer (snd x) lineCount
+                  -- lex string literal
+                  | getStringLiteral s /= (Nothing, 0) = let x = getStringLiteral s in TokenStringLiteral ((certain . fst) x) : lexer (drop ((snd x) + 2) s) lineCount
+                  --if we encounter something that isn't empty and we've not matched it it's an error
+                  | (s /= []) = error ("Lexer error at line " ++ (show lineCount) ++ " around your code:\"" ++ (take 5 s) ++ "\"")
+
+lexer [] lineCount = []
