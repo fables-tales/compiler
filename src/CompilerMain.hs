4232d7c0392d4ae6e7a847ad5e1f8a480577c4c6
Sam Phippen <samphippen@googlemail.com>
2010-09-04 18:19:41 +0100

    make the compiler use the semantic analyser

--- src/CompilerMain.hs
+++ src/CompilerMain.hs
@@ -2,10 +2,12 @@ module Main where
 import Parser
 import Lexer
 import IR
+import Semantics
 import System ( getArgs )
 
 
 main :: IO()
 main = do args <- getArgs;
 		  contents <- readFile (head (args));
-		  putStrLn ((toAssembly . toIrForm . camleParser . lexer) contents);
+          let semanticPair = (verifySemantics . camleParser . lexer) contents;
+          if fst semanticPair == True then putStrLn ((toAssembly . toIrForm) (snd semanticPair)) else error "bad program semantics"
