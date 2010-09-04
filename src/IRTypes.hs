adf287848056cbb2e863b8548bf37ea2fbff09d5
Sam Phippen <samphippen@googlemail.com>
2010-09-04 14:36:27 +0100

    add the ir math types

--- src/IRTypes.hs
+++ src/IRTypes.hs
@@ -5,10 +5,14 @@ data IRForm = WriteInt {register :: Int} | WriteReal {register :: Int}
               | LoadImmediateInt {register,value :: Int} | LoadImmediateReal {register :: Int, fvalue :: Float}
               | WriteString {location :: Int} | DataPseudo Int | Halt | MemoryStore {reg,memoryAddr :: Int}
               | MemoryLoad {reg,memoryAddr :: Int} | IToR {reg :: Int} | RToI {reg :: Int}
-              | IRAdd {r1,r2,r3 :: Int}
-              | IRDiv {r1,r2,r3 :: Int}
-              | IRMul {r1,r2,r3 :: Int}
-              | IRSub {r1,r2,r3 :: Int}
+              | AddReal {r1,r2,r3 :: Int}
+              | SubReal {r1,r2,r3 :: Int}
+              | MulReal {r1,r2,r3 :: Int}
+              | DivReal {r1,r2,r3 :: Int}
+              | AddInt {r1,r2,r3 :: Int}
+              | SubInt {r1,r2,r3 :: Int}
+              | MulInt {r1,r2,r3 :: Int}
+              | DivInt {r1,r2,r3 :: Int}
               deriving (Show, Read, Eq)
 
 data IRExpType = TInt | TReal deriving (Show, Read, Eq, Ord)
