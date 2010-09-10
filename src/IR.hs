cf8464c28b84f1aa0ee0595b8b0d50797299e2da
Sam Phippen <samphippen@googlemail.com>
2010-09-10 20:48:27 +0100

    add an implementation of if that needs testing
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/IR.hs
+++ src/IR.hs
@@ -8,6 +8,7 @@ import IRTypes
 import ParserTypes
 
 --convert an expression to ir form, put result in second integer arg
+--the offset parameter is the offset of the variable block of memory after the stirng block
 expToIr :: Expression -> [Declaration] -> Int -> Int -> ([IRForm],IRExpType)
 expToIr (TermConstant (IntegerLiteral a)) decs offest resultReg = ([LoadImmediateInt resultReg a], TInt)
 expToIr (TermConstant (RealLiteral a)) decs offset resultReg = ([LoadImmediateReal resultReg a],TReal)
@@ -22,28 +23,37 @@ expToIr (Op a child1 child2) decs offset resultReg = let
                                                          irForOp a resultReg richest (snd c1Pair) (snd c2Pair), richest)
 
 --convert array of statements to ir form
-_toIrForm :: [Statement] -> [((String, Int), [IRForm])] -> [Declaration] -> [IRForm]
+_toIrForm :: [Statement] -> [((String, Int), [IRForm])] -> [Declaration] -> Int -> [IRForm]
 
-_toIrForm (WriteExp a : rest) stringTable decs = let irPair = expToIr a decs (stringSectionSize stringTable) 0 in
+_toIrForm (WriteExp a : rest) stringTable decs labelCount = let irPair = expToIr a decs (stringSectionSize stringTable) 0 in
                                             fst irPair
                                             ++ (if snd irPair == TReal then WriteReal 0 else WriteInt 0)
-                                            :  _toIrForm rest stringTable decs
+                                            :  _toIrForm rest stringTable decs labelCount
 
-_toIrForm (WriteLn : rest) stringTable decs = WriteString 0 : _toIrForm rest stringTable decs
-_toIrForm (WriteS s : rest) stringTable decs = WriteString (findStringLocation stringTable s) : _toIrForm rest stringTable decs
+_toIrForm (WriteLn : rest) stringTable decs labelCount = WriteString 0 : _toIrForm rest stringTable decs labelCount
+_toIrForm (WriteS s : rest) stringTable decs labelCount = WriteString (findStringLocation stringTable s) : _toIrForm rest stringTable decs labelCount
 
-_toIrForm (Assign id exp : rest) stringTable decs = let
+_toIrForm (Assign id exp : rest) stringTable decs labelCount = let
                                                     dataOffset = stringSectionSize stringTable
                                                     pair = expToIr exp decs dataOffset 0
                                                   in
-                                           fst pair ++
-										   [RToI 0 | snd pair == TReal && getType decs id == TInt] ++
-										   [IToR 0 | snd pair == TInt && getType decs id == TReal] ++
-                                           MemoryStore 0 (varLocation dataOffset decs id) :
-                                           _toIrForm rest stringTable decs
+                                               fst pair ++
+                                               [RToI 0 | snd pair == TReal && getType decs id == TInt] ++
+                                               [IToR 0 | snd pair == TInt && getType decs id == TReal] ++
+                                               MemoryStore 0 (varLocation dataOffset decs id) :
+                                               _toIrForm rest stringTable decs labelCount
 
-_toIrForm (a : rest) stringTable decs = _toIrForm rest stringTable decs
-_toIrForm [] stringTable decs = Halt : writeTable stringTable ++ allocateDeclarations decs
+_toIrForm (If comparison statements : rest) stringTable decs labelCount =
+                                                         let
+                                                            labelName = "if" ++ labelCount
+                                                            ir = _toIrForm statements stringTable decs (labelCount + 1)
+                                                         in
+                                                         serializeComparison comparison labelName ++
+                                                         Label (labelName ++ "in") : ir ++
+                                                         Label (labelName ++ "aft") : _toIrForm rest decs (labelCount + 1 + numLabels ir)
+
+_toIrForm (a : rest) stringTable decs labelCount = _toIrForm rest stringTable decs
+_toIrForm [] stringTable decs labelCount = Halt : writeTable stringTable ++ allocateDeclarations decs
 
 --convert program to ir form
 toIrForm :: Program -> [IRForm]
@@ -53,8 +63,8 @@ _toAssembly :: [IRForm] -> String
 _toAssembly ((WriteInt {register = a}) : rest) = ";writing register r" ++ show a ++ "\nWR " ++ regToString a ++ "\n" ++ _toAssembly rest
 _toAssembly ((WriteReal {register = a}) : rest) = ";writing register r" ++ show a ++ "\nWRR " ++ regToString a ++ "\n" ++ _toAssembly rest
 _toAssembly ((LoadImmediateInt {register = reg, value = v}) : rest) = ";loading immediate " ++ show v ++ " into register r" ++ show reg
-															++ zero reg ++ "\n"
-															++ "ADDI " ++ regToString reg ++ " " ++ regToString reg ++ " " ++ show v ++ "\n" ++ _toAssembly rest
+                                                            ++ zero reg ++ "\n"
+                                                            ++ "ADDI " ++ regToString reg ++ " " ++ regToString reg ++ " " ++ show v ++ "\n" ++ _toAssembly rest
 _toAssembly ((LoadImmediateReal {register = reg, fvalue = v}) : rest) = ";loading immediate " ++ show v ++ "into register r" ++ show reg
                                                                        ++ "\nMOVIR " ++ regToString reg ++ " " ++ show v ++ "\n" ++ _toAssembly rest
 _toAssembly (WriteString {location = loc} : rest) = "WRS " ++ show loc ++ "\n" ++ _toAssembly rest
--- src/IRTypes.hs
+++ src/IRTypes.hs
@@ -8,8 +8,12 @@ data IRForm = WriteInt {register :: Int} | WriteReal {register :: Int}
               | LoadImmediateInt {register,value :: Int} | LoadImmediateReal {register :: Int, fvalue :: Float}
               | WriteString {location :: Int} | DataPseudo Int | Halt | MemoryStore {reg,memoryAddr :: Int}
               | MemoryLoad {reg,memoryAddr :: Int} | IToR {reg :: Int} | RToI {reg :: Int}
-              | DoMath {op :: MathOp, r1,r2,r3 :: Int}
+              | DoMath {op :: MathOp, r1,r2,r3 :: Int} | Label String
+              --condition to branch on and register to compare
+              | Br {cond :: BrCondition, reg,location :: Int}
               deriving (Show, Read, Eq)
+
+data BrCondition = LtZ | LtZR | GeqZ | GeqZR | EqZ | EqZR | NEqZ | NEqZR deriving (Show, Read, Eq)
 
 data IRExpType = TInt | TReal deriving (Show, Read, Eq, Ord)
 
