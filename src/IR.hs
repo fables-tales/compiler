fd809a83aad2e71cd207bfc26a540784b8473cb3
Sam Phippen <samphippen@googlemail.com>
2010-09-12 21:07:41 +0100

    implement conditionals
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/IR.hs
+++ src/IR.hs
@@ -22,11 +22,62 @@ expToIr (Op a child1 child2) decs offset resultReg = let
                                                          fst c2Pair ++
                                                          irForOp a resultReg richest (snd c1Pair) (snd c2Pair), richest)
 
-numLabels :: [IRForm] -> Int
-numLabels a = sum (map (\x -> if x == Label then 1 else 0) a)
+isLabel :: IRForm -> Bool
+isLabel (Label a) = True
+isLabel _ = False
 
-serializeComparison :: Comparison -> String -> String -> [IRForm]
-serializeComparison (Comparison rel e1 e2) labelTrue labelFalse = let actualComparison = getComparisonFor rel in if fst actualComparison then expToIr
+numLabels :: [IRForm] -> Int
+numLabels a = sum (map (\x -> if isLabel x then 1 else 0) a)
+
+cmpExps :: Expression -> Expression -> [Declaration] -> Int -> [IRForm]
+cmpExps e1 e2 decs offset = let
+                             se1 = expToIr e1 decs offset 0
+                             se2 = expToIr e2 decs offset 1
+                            in
+                             fst se1 ++
+                             fst se2 ++
+                             checkedCast (snd se1) (snd se2) 1 ++
+                             checkedCast (snd se2) (snd se1) 0 ++
+                             irForOp Subtract 0 (selectRichestType (snd se1) (snd se2)) (snd se1) (snd se2)
+
+
+--mapping of expressions into branches, note that to save instructions
+--expressions are reversed if we don't have a native way of executing the
+--specific comparison
+cmpTable = [
+            ((RelLess,TReal),      (True,LtZR)),
+            ((RelLess,TInt),       (True,LtZ)),
+            ((RelGreaterEq,TReal), (True,GeqZR)),
+            ((RelGreaterEq,TInt),  (True,GeqZ)),
+            ((RelLessEq,TReal),    (False,GeqZR)),
+            ((RelLessEq,TInt),     (False,GeqZ)),
+            ((RelGreater,TReal),   (False,LtZR)),
+            ((RelGreater,TInt),    (False,LtZ)),
+            ((RelEq,TReal),        (True, EqZR)),
+            ((RelEq,TInt),         (True, EqZ)),
+            ((RelNeq,TReal),       (True, NEqZR)),
+            ((RelNeq,TInt),        (True, NEqZ))
+           ]
+
+_getComparisonFor :: Relation -> IRExpType -> (Bool, BrCondition)
+_getComparisonFor rel t = let lookup = (rel,t) in (snd . fromJust) (find ((== lookup) . fst) cmpTable)
+
+getComparisonFor :: Relation -> Expression -> Expression -> [Declaration] -> Int -> (Bool, BrCondition)
+getComparisonFor rel e1 e2 decs offset = let
+                                            se1 = expToIr e1 decs offset 0
+                                            se2 = expToIr e2 decs offset 0
+                                         in
+                                            _getComparisonFor rel (selectRichestType (snd se1) (snd se1))
+
+serializeComparison :: Comparison -> String -> Bool -> [Declaration] -> Int -> [IRForm]
+serializeComparison (Comparison rel e1 e2) labelBranch branchOn decs offset = let
+                                                                        rel2 = if branchOn then rel else opposite rel
+                                                                        actualComparison = getComparisonFor rel2 e1 e2 decs offset
+                                                                        flip = (not . fst) actualComparison
+                                                                      in
+                                                                      (if not flip then cmpExps e1 e2 decs offset
+                                                                      else cmpExps e2 e1 decs offset) ++
+                                                                      [Br (snd actualComparison) 0 labelBranch]
 
 --convert array of statements to ir form
 _toIrForm :: [Statement] -> [((String, Int), [IRForm])] -> [Declaration] -> Int -> [IRForm]
@@ -51,19 +102,49 @@ _toIrForm (Assign id exp : rest) stringTable decs labelCount = let
 
 _toIrForm (If comparison statements : rest) stringTable decs labelCount =
                                                          let
-                                                            labelName = "if" ++ labelCount
+                                                            labelName = "if" ++ show labelCount
                                                             ir = _toIrForm statements stringTable decs (labelCount + 1)
                                                          in
-                                                         serializeComparison comparison (labelName ++ "in") (labelName ++ "aft") ++
+                                                         --on false, branch to after the if statement
+                                                         serializeComparison comparison (labelName ++ "aft") False decs (stringSectionSize stringTable) ++
                                                          Label (labelName ++ "in") : ir ++
-                                                         Label (labelName ++ "aft") : _toIrForm rest decs (labelCount + 1 + numLabels ir)
+                                                         Label (labelName ++ "aft") : _toIrForm rest stringTable decs (labelCount + 1 + numLabels ir)
+_toIrForm (IfElse comparison s1 s2 : rest) stringTable decs labelCount =
+                                                        let
+                                                            labelName = "if" ++ show labelCount
+                                                            ir1 = _toIrForm s1 stringTable decs (labelCount + 1)
+                                                            ir2 = _toIrForm s2 stringTable decs (labelCount + 1 +  numLabels ir1)
+                                                        in
+                                                        serializeComparison comparison (labelName ++ "else") False decs (stringSectionSize stringTable) ++
+                                                        Label (labelName ++ "in") : ir1 ++
+                                                        [Zero 0,Br EqZ 0 (labelName ++ "aft")] ++
+                                                        Label (labelName ++ "else") : ir2
+                                                        ++ Label (labelName ++ "aft") :
+                                                        _toIrForm rest stringTable decs (labelCount + 1 + numLabels ir2)
 
-_toIrForm (a : rest) stringTable decs labelCount = _toIrForm rest stringTable decs
-_toIrForm [] stringTable decs labelCount = Halt : writeTable stringTable ++ allocateDeclarations decs
+_toIrForm (a : rest) stringTable decs labelCount = _toIrForm rest stringTable decs labelCount
+_toIrForm [] stringTable decs labelCount = []
+
+
+brTable :: [(BrCondition,String)]
+brTable =  [
+             (LtZR,  "BLTZR"),
+             (LtZ,   "BLTZ"),
+             (GeqZ,  "BGEZ"),
+             (GeqZR, "BGEZR"),
+             (EqZ,   "BEQZ"),
+             (EqZR,  "BEQZR"),
+             (NEqZ,  "BNEZ"),
+             (NEqZR, "BNEZR")
+           ]
+
+brAsm :: BrCondition -> String
+brAsm a = (snd . fromJust) (find ((== a) . fst) brTable)
 
 --convert program to ir form
 toIrForm :: Program -> [IRForm]
-toIrForm (Program name (Block decs statements)) = _toIrForm statements ((("\n",0),[DataPseudo 10, DataPseudo 0]) : dataMapTable statements 2) decs
+toIrForm (Program name (Block decs statements)) = let table = (("\n",0),[DataPseudo 10, DataPseudo 0]) : dataMapTable statements 2 in
+                                                  _toIrForm statements table decs 0 ++ (Halt : writeTable table ++ allocateDeclarations decs)
 
 _toAssembly :: [IRForm] -> String
 _toAssembly ((WriteInt {register = a}) : rest) = ";writing register r" ++ show a ++ "\nWR " ++ regToString a ++ "\n" ++ _toAssembly rest
@@ -81,6 +162,9 @@ _toAssembly (MemoryLoad reg addr : rest) = let spare = findSpareReg reg in zero 
 _toAssembly (IToR reg : rest) = "ITOR " ++ regToString reg ++ " " ++ regToString reg ++ "\n" ++ _toAssembly rest
 _toAssembly (RToI reg : rest) = "RTOI " ++ regToString reg ++ " " ++ regToString reg ++ "\n" ++ _toAssembly rest
 _toAssembly (DoMath op a b c : rest) = toAsm op ++ " " ++ regToString a ++ " " ++ regToString b ++ " " ++ regToString c ++ "\n" ++ _toAssembly rest
+_toAssembly (Br cond reg label : rest) = brAsm cond ++ " " ++  regToString reg ++ " " ++ label ++ "\n" ++  _toAssembly rest
+_toAssembly (Zero reg : rest) = zero reg ++ "\n" ++ _toAssembly rest
+_toAssembly (Label a : rest) = a ++ ":\n" ++ _toAssembly rest
 _toAssembly (_ : rest) = _toAssembly rest
 _toAssembly [] = []
 
--- src/IRTypes.hs
+++ src/IRTypes.hs
@@ -9,8 +9,9 @@ data IRForm = WriteInt {register :: Int} | WriteReal {register :: Int}
               | WriteString {location :: Int} | DataPseudo Int | Halt | MemoryStore {reg,memoryAddr :: Int}
               | MemoryLoad {reg,memoryAddr :: Int} | IToR {reg :: Int} | RToI {reg :: Int}
               | DoMath {op :: MathOp, r1,r2,r3 :: Int} | Label String
+              | Zero {reg :: Int}
               --condition to branch on and register to compare
-              | Br {cond :: BrCondition, reg,location :: Int}
+              | Br {cond :: BrCondition, reg :: Int, label :: String}
               deriving (Show, Read, Eq)
 
 data BrCondition = LtZ | LtZR | GeqZ | GeqZR | EqZ | EqZR | NEqZ | NEqZR deriving (Show, Read, Eq)
