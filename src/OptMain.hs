fa583825a18a1a7407738971eb55cc1a89d72ab0
Sam Phippen <samphippen@googlemail.com>
2010-09-19 20:25:40 +0100

    add the optimiser
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/OptMain.hs
+++ src/OptMain.hs
@@ -11,5 +11,5 @@ import Optimiser
 main :: IO()
 main = do args <- getArgs;
 		  contents <- readFile (head args);
-          let semanticPair = (parseOptimise . verifySemantics . camleParser . lexer) contents;
+          let semanticPair = (verifySemantics . parseOptimise . camleParser . lexer) contents;
           if fst semanticPair then putStrLn ((toAssembly . toIrForm) (snd semanticPair)) else error "bad program semantics"
--- src/Optimiser.hs
+++ src/Optimiser.hs
@@ -11,11 +11,24 @@ multiplyByOne (Op a b c) = Op a (multiplyByOne b) (multiplyByOne c)
 multiplyByOne (TermConstant a) = TermConstant a
 multiplyByOne (TermVar a) = TermVar a
 
+breakIt :: Expression -> Expression
+breakIt (Op a b c) = Op Add (breakIt b) (breakIt c)
+breakIt (TermConstant a) = TermConstant a
+breakIt (TermVar a) = TermVar a
+
+addZero :: Expression -> Expression
+addZero (Op Add a (TermConstant (IntegerLiteral 0))) = addZero a
+addZero (Op Add (TermConstant (IntegerLiteral 0)) a) = addZero a
+addZero (Op Add a (TermConstant (RealLiteral 0))) = addZero a
+addZero (Op Add (TermConstant (RealLiteral 0)) a) = addZero a
+addZero (Op a b c) = Op a (addZero b) (addZero c)
+addZero (TermConstant a) = TermConstant a
+addZero (TermVar a) = TermVar a
 
 optTrans :: Expression -> Expression
-optTrans a = a
+optTrans = multiplyByOne . addZero
 
 parseOptimise :: Program -> Program
-parseOptimise prog = let p = transformExpressions optTrans prog in if p == prog then p else parseOptimise p
+parseOptimise prog = let p = transformExpressions optTrans prog in if (p == prog) then p else parseOptimise p
 
 
