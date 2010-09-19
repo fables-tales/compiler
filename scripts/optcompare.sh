c8695f6093b4890d4e799657da032a7bc23c3e41
Sam Phippen <samphippen@googlemail.com>
2010-09-19 20:26:50 +0100

    fix optcompare.sh
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- scripts/optcompare.sh
+++ scripts/optcompare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./build/compiler $1 > $2
-echo $3 | ./assmule $4 $2
+echo $3 | ./assmule $4 $2 | tail -n 3
 ./build/opt-compiler $1 > $2
-echo $3 | ./assmule $4 $2
+echo $3 | ./assmule $4 $2 | tail -n 3
