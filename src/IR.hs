d65241fe40f590d2a6362ef24a678377f47ef631
Sam Phippen <samphippen@googlemail.com>
2010-09-26 17:51:00 +0100

    zero out r0 at the start of the program, never write it
    this allows for much much better code routines to be generated
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/IR.hs
+++ src/IR.hs
@@ -32,14 +32,14 @@ numLabels a = sum (map (\x -> if isLabel x then 1 else 0) a)
 
 cmpExps :: Expression -> Expression -> [Declaration] -> Int -> [IRForm]
 cmpExps e1 e2 decs offset = let
-                             se1 = expToIr e1 decs offset 0
-                             se2 = expToIr e2 decs offset 1
+                             se1 = expToIr e1 decs offset 1
+                             se2 = expToIr e2 decs offset 2
                             in
                              fst se1 ++
                              fst se2 ++
-                             checkedCast (snd se1) (snd se2) 1 ++
-                             checkedCast (snd se2) (snd se1) 0 ++
-                             irForOp Subtract 0 (selectRichestType (snd se1) (snd se2)) (snd se1) (snd se2)
+                             checkedCast (snd se1) (snd se2) 2 ++
+                             checkedCast (snd se2) (snd se1) 1 ++
+                             irForOp Subtract 1 (selectRichestType (snd se1) (snd se2)) (snd se1) (snd se2)
 
 
 --mapping of expressions into branches, note that to save instructions
@@ -78,14 +78,14 @@ serializeComparison (Comparison rel e1 e2) labelBranch branchOn decs offset = le
                                                                       in
                                                                       (if not flip then cmpExps e1 e2 decs offset
                                                                       else cmpExps e2 e1 decs offset) ++
-                                                                      [Br (snd actualComparison) 0 labelBranch]
+                                                                      [Br (snd actualComparison) 1 labelBranch]
 
 --convert array of statements to ir form
 _toIrForm :: [Statement] -> [((String, Int), [IRForm])] -> [Declaration] -> Int -> [IRForm]
 
-_toIrForm (WriteExp a : rest) stringTable decs labelCount = let irPair = expToIr a decs (stringSectionSize stringTable) 0 in
+_toIrForm (WriteExp a : rest) stringTable decs labelCount = let irPair = expToIr a decs (stringSectionSize stringTable) 1 in
                                             fst irPair
-                                            ++ (if snd irPair == TReal then WriteReal 0 else WriteInt 0)
+                                            ++ (if snd irPair == TReal then WriteReal 1 else WriteInt 1)
                                             :  _toIrForm rest stringTable decs labelCount
 
 _toIrForm (WriteLn : rest) stringTable decs labelCount = WriteString 0 : _toIrForm rest stringTable decs labelCount
@@ -93,12 +93,12 @@ _toIrForm (WriteS s : rest) stringTable decs labelCount = WriteString (findStrin
 
 _toIrForm (Assign id exp : rest) stringTable decs labelCount = let
                                                     dataOffset = stringSectionSize stringTable
-                                                    pair = expToIr exp decs dataOffset 0
+                                                    pair = expToIr exp decs dataOffset 1
                                                   in
                                                fst pair ++
-                                               [RToI 0 | snd pair == TReal && getType decs id == TInt] ++
-                                               [IToR 0 | snd pair == TInt && getType decs id == TReal] ++
-                                               MemoryStore 0 (varLocation dataOffset decs id) :
+                                               [RToI 1 | snd pair == TReal && getType decs id == TInt] ++
+                                               [IToR 1 | snd pair == TInt && getType decs id == TReal] ++
+                                               MemoryStore 1 (varLocation dataOffset decs id) :
                                                _toIrForm rest stringTable decs labelCount
 
 _toIrForm (If comparison statements : rest) stringTable decs labelCount =
@@ -118,7 +118,7 @@ _toIrForm (IfElse comparison s1 s2 : rest) stringTable decs labelCount =
                                                         in
                                                         serializeComparison comparison (labelName ++ "else") False decs (stringSectionSize stringTable) ++
                                                         Label (labelName ++ "in") : ir1 ++
-                                                        [Zero 0,Br EqZ 0 (labelName ++ "aft")] ++
+                                                        [Zero 1,Br EqZ 1 (labelName ++ "aft")] ++
                                                         Label (labelName ++ "else") : ir2
                                                         ++ Label (labelName ++ "aft") :
                                                         _toIrForm rest stringTable decs (labelCount + 1 + numLabels ir2)
@@ -136,7 +136,7 @@ _toIrForm (Read id : rest) stringTable decs labelCount = let
                                                             idType = getType decs id
                                                             idLocation = varLocation (stringSectionSize stringTable) decs id
                                                          in
-                                                         (if idType == TInt then ReadInt else ReadFloat) 0 idLocation : _toIrForm rest stringTable decs labelCount
+                                                         (if idType == TInt then ReadInt else ReadFloat) 1 idLocation : _toIrForm rest stringTable decs labelCount
 
 _toIrForm (a : rest) stringTable decs labelCount = _toIrForm rest stringTable decs labelCount
 _toIrForm [] stringTable decs labelCount = []
@@ -159,22 +159,21 @@ brAsm a = (snd . fromJust) (find ((== a) . fst) brTable)
 
 --convert program to ir form
 toIrForm :: Program -> [IRForm]
+toIrForm (Program name (Block decs [])) = [Halt]
 toIrForm (Program name (Block decs statements)) = let table = (("\n",0),[DataPseudo 10, DataPseudo 0]) : dataMapTable statements 2 in
-                                                  _toIrForm statements table decs 0 ++ (Halt : writeTable table ++ allocateDeclarations decs)
+                                                  Zero 0 : _toIrForm statements table decs 0 ++ (Halt : writeTable table ++ allocateDeclarations decs)
 
 --convert ir to assembly data structure
 _toAssembly :: [IRForm] -> [Assembly]
 _toAssembly ((WriteInt {register = a}) : rest) = WR a : _toAssembly rest
 _toAssembly ((WriteReal {register = a}) : rest) = WRR a : _toAssembly rest
-_toAssembly ((LoadImmediateInt {register = reg, value = v}) : rest) = XOR reg reg reg : ADDI reg reg v : _toAssembly rest
+_toAssembly ((LoadImmediateInt {register = reg, value = v}) : rest) = ADDI reg 0 v : _toAssembly rest
 _toAssembly ((LoadImmediateReal {register = reg, fvalue = v}) : rest) = MOVIR reg v : _toAssembly rest
 _toAssembly (WriteString {location = loc} : rest) = WRS loc : _toAssembly rest
 _toAssembly (Halt : rest) = HALT : _toAssembly rest
 _toAssembly (DataPseudo a : rest) = DATA a : _toAssembly rest
-_toAssembly (MemoryStore reg addr : rest) = let spare = findSpareReg reg in XOR spare spare spare
-                                            : STORE reg spare addr : _toAssembly rest
-_toAssembly (MemoryLoad reg addr : rest) = let spare = findSpareReg reg in XOR spare spare spare :
-                                             LOAD reg spare addr : _toAssembly rest
+_toAssembly (MemoryStore reg addr : rest) = let spare = 0 in STORE reg spare addr : _toAssembly rest
+_toAssembly (MemoryLoad reg addr : rest) = let spare = 0 in LOAD reg spare addr : _toAssembly rest
 _toAssembly (IToR reg : rest) = ITOR reg reg : _toAssembly rest
 _toAssembly (RToI reg : rest) = RTOI reg reg : _toAssembly rest
 _toAssembly (DoMath op a b c : rest) = (toAsm op False) a b c : _toAssembly rest
